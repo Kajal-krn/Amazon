7e1ed7652ac4e4feabd7fd7d31e6d234e0b95604
Kajal-krn <kajalkiran191@gmail.com>
2023-05-23 04:18:00 +0530

    specific err msg

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err,req,res,next) => {
         err = new ErrorHandler(message,400);
     }
 
+    // mongoose duplicate key
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400);
+    }
+
+    // wrong Jwt error
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json Web Token is invalid, Please try again`;
+        err = new ErrorHandler(message,400);
+    }
+
+    // Jwt expire error
+    if(err.name === "TokenExpiredError"){
+        const message = "Json Web Token expired, Please try again";
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success : false,
         message : err.message
