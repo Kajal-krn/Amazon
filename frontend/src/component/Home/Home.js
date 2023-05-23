ea590fbf7042520bff83c58a9d2a9b227b7d52ca
Aman9798 <amaniitg2001@gmail.com>
2023-05-24 02:35:10 +0530

    dummy product display

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -3,6 +3,13 @@ import {CgMouse} from "react-icons/all"
 import "./Home.css"
 import Product from '../Product/Product.js'
 
+const product = {
+    name : "Black Tshirt",
+    images : [{ url : "https://5.imimg.com/data5/GX/XW/MY-67450080/mens-black-plain-t-shirt-500x500.jpg" }],
+    price : "$99",
+    _id : "Aman"
+}
+
 const Home = () => {
     return (
         <Fragment>
@@ -10,7 +17,7 @@ const Home = () => {
                 <p>Welcome to Amazon</p>
                 <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-                <a href="#container">
+                 <a href="#container">  {/*refering to container below */}
                     <button>
                         Scroll <CgMouse />
                     </button>
@@ -20,7 +27,14 @@ const Home = () => {
             <h2 className="homeHeading">Featured Products</h2>
 
             <div className="container" id="container">
-                <Product />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
             </div>
 
         </Fragment>
