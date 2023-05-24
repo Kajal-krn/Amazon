a57bd8ee07744965ef838dcd3d3dfa426e9d0a5b
Aman9798 <amaniitg2001@gmail.com>
2023-05-25 02:08:46 +0530

    pagination

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,26 +1,33 @@
-import React,{Fragment, useEffect} from 'react';
+import React,{Fragment, useEffect, useState} from 'react';
 import {useDispatch,useSelector} from "react-redux";
 import {getProducts,clearErrors} from "../../actions/productAction.js";
 import Loader from "../layout/Loader/Loader.js";
 import ProductCard from "./ProductCard.js";
 import {useAlert} from "react-alert";
+import Pagination from "react-js-pagination";
 import "./Product.css";
 
 const Products = ({match}) => {
 
+    const [currentPage, setCurrentPage] = useState(1);
+
     const alert = useAlert();
     const dispatch = useDispatch();
-    const {loading,error,products,productsCount} = useSelector(state => state.products)
+    const {loading,error,products,productsCount,resultPerPage} = useSelector(state => state.products)
 
     const keyword = match.params.keyword;
+
+    const setCurrentPageNo = (e) => {
+        setCurrentPage(e);
+    }
 
     useEffect(() => {
         if(error){
             alert.error(error);
             dispatch(clearErrors())
         }
-        dispatch(getProducts(keyword))
-    },[dispatch,alert,error,keyword])
+        dispatch(getProducts(keyword,currentPage))
+    },[dispatch,alert,error,keyword,currentPage])
 
     return (
         <Fragment>{loading ? (<Loader />) : (
@@ -33,6 +40,24 @@ const Products = ({match}) => {
                     ))}
                 </div>
 
+                {resultPerPage < productsCount && (
+                    <div className="paginationBox">
+                        <Pagination
+                            activePage={currentPage} 
+                            itemsCountPerPage={resultPerPage}
+                            totalItemsCount={productsCount}
+                            onChange={setCurrentPageNo}
+                            nextPageText="Next"
+                            prevPageText="Prev"
+                            firstPageText="1st"
+                            lastPageText="Last"
+                            itemClass="page-item"
+                            linkClass="page-link"
+                            activeClass="pageItemActive"
+                            activeLinkClass="pageLinkActive"
+                        />
+                    </div>
+                )}
             </Fragment>
         )}</Fragment>
     )
