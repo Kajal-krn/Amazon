3bd0f64c7d702054ad77b5bcd9dcc26a6965e062
Kajal-krn <kajalkiran191@gmail.com>
2023-05-31 01:43:39 +0530

    Deployment

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,12 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const ErrorMiddleware = require("./middleware/error");
 const dotenv = require("dotenv");
+const path = require("path")
 
 //config
-dotenv.config({ path: "backend/config/config.env" });
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    dotenv.config({ path: "backend/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +27,12 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+})
 
 // middleware for error
 app.use(ErrorMiddleware);
